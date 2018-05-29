5f19f2f0d0f1d64385b0e21ed55349205658edd6
AleksandraOrzechowska <aorzechowska_999@wp.pl>
2018-05-29 17:42:48 +0200

    Checking Codecov

---
+++ src/test/java/skeleton/TestB.java
@@ -0,0 +1,12 @@
+package skeleton;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestB {
+
+    @Test
+   public void test() {
+               Assert.assertTrue(true);
+           }
+}
